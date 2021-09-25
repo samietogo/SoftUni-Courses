f17df261b3e668754c37cec1a911669081aa976e
Samuil Tsvetanov <sami.etogo@gmail.com>
2021-09-25 06:38:29 +0300

    New task

--- JavaScript Advanced/5 ARRAYS AND NESTED ARRAYS/03.js
+++
@@ -1,9 +0,0 @@
-// Sum First Last
-function solve(arr) {
-    let first = Number([...arr].shift());
-    let last = Number([...arr].pop());
-    return first + last;
-}
-
-console.log(solve(['20', '30', '40']));
-console.log(solve(['5', '10']));
--- JavaScript Advanced/5 ARRAYS AND NESTED ARRAYS/06.js
+++
@@ -1,7 +0,0 @@
-// Bigger Half
-function solve(arr) {
-    return arr.sort((a, b) => a - b).slice(arr.length / 2);
-}
-
-console.log(solve([4, 7, 2, 5]));
-console.log(solve([3, 19, 14, 7, 2, 19, 6]));
