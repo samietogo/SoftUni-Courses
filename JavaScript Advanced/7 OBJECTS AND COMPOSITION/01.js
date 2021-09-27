08e19de5252e13c617e0f7b91c71a3da382f4cc1
Samuil Tsvetanov <sami.etogo@gmail.com>
2021-09-27 21:15:25 +0300

    New task

---
+++ JavaScript Advanced/7 OBJECTS AND COMPOSITION/01.js
@@ -0,0 +1,12 @@
+function cityRecord(name, population, treasury) {
+    let result = {
+        name,
+        population,
+        treasury
+    }
+
+    return result
+}
+
+console.log(cityRecord('Tortuga', 7000, 15000));
+console.log(cityRecord('Santo Domingo', 12000, 23500));
