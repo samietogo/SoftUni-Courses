b5014d60121e8f9f7143ef4ade184b0a8d1cbc97
Samuil Tsvetanov <sami.etogo@gmail.com>
2021-09-28 22:01:49 +0300

    New task

---
+++ JavaScript Advanced/7 OBJECTS AND COMPOSITION/03.js
@@ -0,0 +1,21 @@
+function cityRecord(name, population, treasury) {
+    let result = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percent) {
+            this.population += Math.floor(this.population * (percent / 100));
+        },
+        applyRecession(percent) {
+            this.treasury -= Math.ceil(this.treasury * (percent / 100));
+        }
+    }
+
+    return result;
+}
+
+console.log(cityRecord('Tortuga', 7000, 15000));
---
+++ JavaScript Advanced/7 OBJECTS AND COMPOSITION/cooking.js
@@ -0,0 +1,12 @@
+// function cookingByNumbers(num, a, b, c, d, e) {
+//     const operation = {
+//         chop(a) {a = a / 2}
+//     }
+// }
+
+const operation = {
+    chop(a) {a = a / 2}
+}
+let a = 10;
+
+console.log(operation.chop(a));
